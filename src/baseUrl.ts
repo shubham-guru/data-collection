f6b5cfa14daafb64f6ba676506eae1e2ca53dfd1
nakuljoshi <nakul@xagroup.com>
2022-10-09 04:12:33 +0530

    issues fixed

--- src/baseUrl.ts
+++ src/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const url = 'http://20.219.13.163:8073'
-export const url = 'http://127.0.0.1:8073'
+export const url = 'http://20.219.13.163:8073'
+// export const url = 'http://127.0.0.1:8073'
